81a00ae8216b42dd76d456adf518f02913ee23c6
fairuzulum <usleunmdi32@gmail.com>
2025-07-13 10:58:42 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -82,7 +82,7 @@ function Footer() {
                   <p className="text-gray-400 text-sm sm:text-base">
                     Dusun Malabar,
                     <br />
-                    Desa/Kelurahan Sidamulih, Kec. Pamarican
+                    Desa/Kelurahan Sidamulih, Kecamatan Pamarican
                     <br />
                     Kab. Ciamis Prov. Jawa Barat
                   </p>
